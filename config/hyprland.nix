dc18053524b2ef312d1db05ceddda0b26127fc79
skitimoon <s.kitimoon@gmail.com>
2024-12-06 12:55:56 +0700

    Initial commit

---
+++ config/hyprland.nix
@@ -0,0 +1,154 @@
+{
+  lib,
+  ...
+}:
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = [
+        "DP-1, 1920x1080@144,0x0, 1"
+        "HDMI-A-1, 1920x1080, 1920x0, 1"
+      ];
+
+      "$terminal" = "kitty";
+
+      exec-once = [
+        "floorp"
+        "logseq"
+        "super-productivity"
+        "swaync"
+        "swww-daemon && swww img ~/nix-config/config/wallpapers/benjamin-voros-phIFdC6lA4E-unsplash.jpg"
+        "thunderbird"
+        "waybar"
+      ];
+
+      dwindle = {
+        pseudotile = true;
+        preserve_split = true;
+      };
+
+      general = {
+        gaps_out = 7;
+        border_size = 2;
+
+        "col.active_border" = lib.mkDefault "rgba(33ccffee) rgba(00ff99ee) 45deg";
+        "col.inactive_border" = lib.mkDefault "rgba(595959aa)";
+      };
+
+      decoration = {
+        rounding = 10;
+      };
+
+      misc = {
+        disable_hyprland_logo = true;
+        key_press_enables_dpms = true;
+      };
+
+      master = {
+        new_status = "master";
+      };
+
+      input = {
+        kb_layout = "us,th";
+        kb_options = "grp:ctrl_space_toggle";
+        accel_profile = "flat";
+        scroll_factor = 0.6;
+      };
+
+      cursor = {
+        hide_on_key_press = true;
+      };
+
+      # device = {
+      #   name = "logitech-g305-1";
+      # };
+
+      "$mod" = "SUPER";
+      bind = [
+        "$mod, return, exec, $terminal"
+        "$mod, Q, killactive"
+        "$mod , F, fullscreen"
+        "$mod SHIFT, F, togglefloating"
+
+        "$mod SHIFT, return, exec, $terminal -e yazi"
+        "$mod, backslash, exec, $terminal -e ssh -t server tmux attach -t arbbot"
+        "$mod, bracketright, exec, $terminal -e ssh -t yimserver tmux attach -t bot"
+        "$mod, space, exec, rofi -show drun"
+        "$mod SHIFT, space, exec, rofi -show run"
+        "$mod, C, exec, rofi -show calc -modi calc -no-show-match -no-sort"
+        "$mod, V, exec, rofi -modi clipboard:~/.config/cliphist/cliphist-rofi-img -show clipboard -show-icons"
+        "$mod, grave, exec, swaync-client -t"
+
+        "$mod, W, focusmonitor, DP-1"
+        "$mod, E, focusmonitor, HDMI-A-1"
+        "$mod SHIFT, W, movewindow, mon:DP-1"
+        "$mod SHIFT, E, movewindow, mon:HDMI-A-1"
+
+        # Apps Shortcut
+        "$mod CTRL, V, exec, code"
+
+        "$mod, H, movefocus, l"
+        "$mod, J, movefocus, d"
+        "$mod, K, movefocus, u"
+        "$mod, L, movefocus, r"
+
+        "$mod, 1, focusworkspaceoncurrentmonitor, 1"
+        "$mod, 2, focusworkspaceoncurrentmonitor, 2"
+        "$mod, 3, focusworkspaceoncurrentmonitor, 3"
+        "$mod, 4, focusworkspaceoncurrentmonitor, 4"
+        "$mod, 5, focusworkspaceoncurrentmonitor, 5"
+        "$mod, 6, focusworkspaceoncurrentmonitor, 6"
+        "$mod, 7, focusworkspaceoncurrentmonitor, 7"
+        "$mod, 8, focusworkspaceoncurrentmonitor, 8"
+        "$mod, 9, focusworkspaceoncurrentmonitor, 9"
+        "$mod, 0, focusworkspaceoncurrentmonitor, 10"
+        "$mod CTRL, 1, focusworkspaceoncurrentmonitor, 6"
+        "$mod CTRL, 2, focusworkspaceoncurrentmonitor, 7"
+        "$mod CTRL, 3, focusworkspaceoncurrentmonitor, 8"
+        "$mod CTRL, 4, focusworkspaceoncurrentmonitor, 9"
+        "$mod CTRL, 5, focusworkspaceoncurrentmonitor, 10"
+
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+        "$mod SHIFT, 0, movetoworkspace, 10"
+
+        "$mod, S, togglespecialworkspace, magic"
+        "$mod SHIFT, S, movetoworkspace, special:magic"
+      ];
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+
+      bindel = [
+        ", XF86AudioRaiseVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
+        ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+      ];
+
+      bindl = [
+        ", XF86AudioNext, exec, playerctl next"
+        ", XF86AudioPause, exec, playerctl play-pause"
+        ", XF86AudioPlayPause, exec, playerctl play-pause"
+        ", XF86AudioPrev, exec, playerctl previous"
+      ];
+
+      windowrulev2 = [
+        "workspace 2, class:floorp"
+        "workspace 6, class:thunderbird"
+        "workspace 7, class:Logseq"
+        "workspace 10, class:superProductivity"
+      ];
+
+    };
+  };
+}
---
+++ hosts/phoenix/configuration.nix
@@ -0,0 +1,209 @@
+{ pkgs, host, username, ... }:
+
+{
+  imports = [ ./hardware-configuration.nix ];
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader = {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 7;
+      };
+      efi.canTouchEfiVariables = true;
+    };
+    tmp = {
+      useTmpfs = true;
+      # tmpfsSize = "30%";
+    };
+  };
+
+
+  networking.hostName = host;
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Bangkok";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    supportedLocales = [
+      "en_GB.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "th_TH.UTF-8/UTF-8"
+    ];
+  };
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  programs = {
+    hyprland.enable = true;
+    kdeconnect.enable = true;
+    zsh.enable = true;
+  };
+
+  # Enable sound.
+  services = {
+    kanata = {
+      enable = true;
+      keyboards.default = {
+        extraDefCfg = "process-unmapped-keys yes";
+        config = ''
+          (defsrc
+            f1             f3                 f4             f5         f6         f7
+            tab       w
+                 a    s    d     h    j    k   l
+          )
+          (deflayer default
+            MediaPlayPause MediaTrackPrevious MediaTrackNext VolumeMute VolumeDown VolumeUp
+            @tab      _
+                 _    _    _     _    _    _   _
+          )
+          (deflayer nav
+            _              _                  _              _          _          _
+            _         up
+                 left down right left down up  right
+          )
+          (defalias
+            tab (tap-hold-press 200 200 tab (layer-while-held nav))
+          )
+        '';
+      };
+    };
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  users.users."${username}" = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages = with pkgs; [
+    brightnessctl
+    git
+    neovim
+    pwvucontrol
+    wget
+  ];
+
+  # For Logseq
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-27.3.11"
+  ];
+
+  fonts.packages = [
+    (pkgs.nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+    # pkgs.nerd-fonts.fira-code
+    # pkgs.nerd-fonts.jetbrains-mono
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  stylix = {
+    enable = true;
+    autoEnable = true;
+    cursor = {
+      package = pkgs.nordzy-cursor-theme;  # pkgs.bibata-cursors;
+      name = "Nordzy-cursors";
+      size = 24;
+    };
+    image = ../../config/wallpapers/benjamin-voros-phIFdC6lA4E-unsplash.jpg;
+    polarity = "dark";
+    opacity.terminal = 0.8;
+    fonts = {
+      monospace = {
+        package = pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; };
+        # package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font Mono";
+      };
+      sansSerif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      serif = {
+        package = pkgs.montserrat;
+        name = "Montserrat";
+      };
+      sizes = {
+      applications = 10;
+      terminal = 10;
+      desktop = 8;
+      #   popups = 12;
+      };
+    };
+  };
+
+  # List services that you want to enable:
+  services.syncthing = {
+    enable = true;
+    user = username;
+    dataDir = "/home/${username}";
+  };
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+}

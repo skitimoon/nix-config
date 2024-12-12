2a92d42a2dc2ac4a88ca80cb936f7c3f09a622b4
skitimoon <s.kitimoon@gmail.com>
2024-12-12 16:28:36 +0700

    Refactor home-manager input

--- flake.nix
+++ flake.nix
@@ -6,8 +6,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     stylix.url = "github:danth/stylix/release-24.05";
     # nur = {
     #   url = "github:nix-community/NUR";

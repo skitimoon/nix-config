a3f6d29d57b62df272ab62d68222d7883af08305
skitimoon <s.kitimoon@gmail.com>
2024-12-17 10:17:50 +0700

    Add BTC price module

---
+++ hosts/eagle/macbook-pro.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  # imports =
+  #   [
+  #     <nixos-hardware/common/gpu/24.05-compat.nix>
+  #   ];
+  hardware.facetimehd.enable = true;
+  services.fstrim.enable = true;
+  services.mbpfan.enable = true;
+}

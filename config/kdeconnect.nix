9763c66d90753b673ca7038d034a93bd9178a6d9
skitimoon <s.kitimoon@gmail.com>
2025-07-08 15:52:08 +0700

    Update kde connect

--- config/kdeconnect.nix
+++ config/kdeconnect.nix
@@ -6,11 +6,11 @@
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "kdeconnect";
-  version = "5034";
+  version = "5183";
 
   src = fetchurl {
     url = "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-arm64/kdeconnect-kde-master-${version}-macos-clang-arm64.dmg";
-    sha256 = "sha256-U70PTo8kc9aizY9g/46JO97b68YXJSHwf1X14ksBtLU=";
+    sha256 = "sha256-90h+5zU0f2PLyBwUpgfvt6K/j2Azp6+HkXJ381LdsHA=";
   };
 
   sourceRoot = "KDE Connect.app";

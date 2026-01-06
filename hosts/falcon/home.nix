afec2496309ae040a089058a04cd56748a13324f
skitimoon <s.kitimoon@gmail.com>
2026-01-06 23:48:38 +0700

    falcon: update n8n webhook URL to nnn.my.to, bump stateVersion to 25.11

--- hosts/falcon/home.nix
+++ hosts/falcon/home.nix
@@ -14,7 +14,7 @@
   home = {
     username = "${username}";
     homeDirectory = "/home/${username}";
-    stateVersion = "25.05";
+    stateVersion = "25.11";
   };
 
   programs.home-manager.enable = true;

a9189608abf5fc4db8562dd9db013a728d3b6ea1
skitimoon <s.kitimoon@gmail.com>
2026-01-06 23:40:53 +0700

    config/kitty: add notify_on_cmd_finish, remove clipboard_control

--- config/kitty.nix
+++ config/kitty.nix
@@ -1,13 +1,13 @@
-{
+{pkgs, ...}: {
   programs.kitty = {
     enable = true;
     font.name = "JetBrainsMono Nerd Font Mono";
     settings = {
-      clipboard_control = "write-clipboard write-primary read-clipboard read-primary";
       enable_audio_bell = false;
+      macos_option_as_alt = true;
+      notify_on_cmd_finish = "unfocused";
       scrollback_lines = 50000;
       visual_bell_duration = 0.5;
-      macos_option_as_alt = true;
     };
   };
 }

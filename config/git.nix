eb60ae9f698534439e8e6d7ed36bc49f283cb258
skitimoon <s.kitimoon@gmail.com>
2026-01-28 08:26:41 +0700

    style: format git.nix and mpv.nix
    - Add init.defaultBranch = main to git config
    - Condense lists to single-line format

--- config/git.nix
+++ config/git.nix
@@ -3,18 +3,13 @@
     enable = true;
     lfs.enable = true;
     settings = {
+      init.defaultBranch = "main";
       fetch.prune = true;
       user = {
         email = "s.kitimoon@gmail.com";
         name = "skitimoon";
       };
     };
-    ignores = [
-      ".kiro/"
-      ".stfolder"
-      ".stignore"
-      "Session.vim"
-      "scratchpad.*"
-    ];
+    ignores = [".kiro/" ".stfolder" ".stignore" "Session.vim" "scratchpad.*"];
   };
 }
